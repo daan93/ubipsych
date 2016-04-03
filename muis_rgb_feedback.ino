4c9cfd7263fb75588a7c8c1218cafd163f66f4b5
daan93 <daniel.dewit@outlook.com>
2016-04-03 11:59:23 +0200

    muisklikcode toegevoegd

--- muis_rgb_feedback.ino
+++ muis_rgb_feedback.ino
@@ -1,4 +1,5 @@
 
+char data; // Data received from the serial port
 int signal=A0;
 int value=0;
 int redPin=10;
@@ -12,6 +13,7 @@ bool ledState = false; // state variable for the LED
 void setup()
 {
   Serial.begin(9600);          // opens serial port, sets data rate to 9600 bps
+  establishContact();          // send a byte to establish contact until receiver responds 
   pinMode(signal,INPUT);
   pinMode(redPin, OUTPUT);
   pinMode(greenPin, OUTPUT);
@@ -22,31 +24,49 @@ void setup()
 }
 void loop()
 {
-  delay(500);
-  value=analogRead(signal);
-  Serial.println(value);
-  delay(500);
 
-  //Conditions for different colors
-  if (value>1020)
-  {
-    Serial.println("Green");
-    setColor(0, 255, 0);  // green
-    counter = 0; // reset counter
-  }
-  else if (value<= 1020)
-  {
-    counter += 1000; // update counter
-    Serial.print("Counter: ");
-    Serial.println(counter);
-    if (counter < 10000) {
-      setColor(0,0,255);  // blue
-      Serial.println("Blue");
+  if (Serial.available() > 0) { // If data is available to read,
+    data = Serial.read(); // read it and store it in data
+    Serial.println(data);
+
+    if(data == '1') //if we get a 1
+    {
+      Serial.println("Mouse unlocked");
+      delay(500);
+      value=analogRead(signal);
+      Serial.println(value);
+      delay(500);
+    
+      //Conditions for different colors
+      if (value>1020)
+      {
+        Serial.println("Green");
+        setColor(0, 255, 0);  // green
+        counter = 0; // reset counter
+      }
+      else if (value<= 1020)
+      {
+        counter += 1000; // update counter
+        Serial.print("Counter: ");
+        Serial.println(counter);
+        if (counter < 10000) {
+          setColor(0,0,255);  // blue
+          Serial.println("Blue");
+        }
+        else {
+          setColor(255,0,0); // red
+          Serial.println("Red");
+        }
+      }
     }
-    else {
-      setColor(255,0,0); // red
-      Serial.println("Red");
+    if(data == '0') //if we get a 0
+    {
+      Serial.println("mouse locked");
+      counter = 0; // reset counter
+        
+      delay(100);
     }
+   
   }
 }
   
@@ -61,6 +81,13 @@ void setColor(int red, int green, int blue)
   analogWrite(redPin, red);
   analogWrite(greenPin, green);
   analogWrite(bluePin, blue);  
+}
+
+void establishContact() {
+  while (Serial.available() <= 0) {
+  Serial.println("A");   // send a capital A
+  delay(300);
+  }
 }
 
 
